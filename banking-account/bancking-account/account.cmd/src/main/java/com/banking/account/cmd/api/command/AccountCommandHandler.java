f5e082b71ad3bfe9df1fa7c721017c5abd4752af
Jose Arias <josee.ariasmelo@gmail.com>
2025-01-13 18:09:26 -0300

    feat: se agrega la calse AccountCommandHandler

---
+++ banking-account/bancking-account/account.cmd/src/main/java/com/banking/account/cmd/api/command/AccountCommandHandler.java
@@ -0,0 +1,40 @@
+package com.banking.account.cmd.api.command;
+
+import com.banking.account.cmd.domain.AccountAggregate;
+import com.banking.cqrs.core.handlers.EventSourcingHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AccountCommandHandler implements CommandHanlder {
+    @Autowired
+    private EventSourcingHandler<AccountAggregate> eventSourcingHandler;
+
+    @Override
+    public void handle(OpenAccountCommand command) {
+        var aggregate = new AccountAggregate(command);
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(DepositFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.depositFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(WithdrawFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        if (aggregate.getBalance() < command.getAmount()) {
+            throw new IllegalStateException("Insufficient funds");
+        }
+        aggregate.withdrawFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(CloseAccountCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.closeAccount();
+        eventSourcingHandler.save(aggregate);
+    }
+}
