1be726ccccccc9e11b8700004384392df93d61fb
Jose Arias <josee.ariasmelo@gmail.com>
2025-01-21 10:05:44 -0300

    feat: implementación kafka producer

---
+++ banking-account/bancking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountEventProducer.java
@@ -0,0 +1,17 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.cqrs.core.events.BaseEvent;
+import com.banking.cqrs.core.producers.EventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class AccountEventProducer implements EventProducer {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Override
+    public void produceEvent(String topic, BaseEvent event) {
+        kafkaTemplate.send(topic, event);
+    }
+}
