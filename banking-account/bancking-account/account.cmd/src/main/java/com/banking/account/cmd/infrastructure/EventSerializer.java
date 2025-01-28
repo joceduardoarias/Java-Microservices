ec6344111bdfb80693b5792b239112a10dda90a5
Jose Arias <josee.ariasmelo@gmail.com>
2025-01-28 16:46:09 -0300

    feat: Se agrega serializer custom.

---
+++ banking-account/bancking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/EventSerializer.java
@@ -0,0 +1,20 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+
+public class EventSerializer<T> implements Serializer<T> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error serializing value", e);
+        }
+    }
+}
